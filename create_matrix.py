41c5ae4c6b097d5ef7093eb855f5b35016b675b5
Junya Morioka <mjun@mjunya.com>
2025-11-03 15:48:45 +0900

    v0.4.19

--- create_matrix.py
+++ create_matrix.py
@@ -42,10 +42,10 @@ LINUX_SELF_HOSTED_MATRIX = {
 }
 
 WINDOWS_MATRIX = {
-    "flash-attn-version": ["2.6.3", "2.7.4.post1", "2.8.3"],
+    "flash-attn-version": ["2.7.4.post1", "2.8.3"],
     "python-version": ["3.10", "3.11", "3.12", "3.13"],
-    "torch-version": ["2.9.0"],
-    "cuda-version": ["13.0.1"],
+    "torch-version": ["2.5.1", "2.6.0", "2.7.1", "2.8.0", "2.9.0"],
+    "cuda-version": ["12.4.1", "12.6.3", "12.8.1", "12.9.1", "13.0.1"],
 }
 
 WINDOWS_CODEBUILD_MATRIX = {
@@ -60,11 +60,12 @@ def main():
     print(
         json.dumps(
             {
-                "linux": LINUX_MATRIX,
+                # "linux": LINUX_MATRIX,
+                "linux": False,
                 # "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
                 "linux_self_hosted": False,
-                # "windows": WINDOWS_MATRIX,
-                "windows": False,
+                "windows": WINDOWS_MATRIX,
+                # "windows": False,
                 # "windows_code_build": WINDOWS_CODEBUILD_MATRIX,
                 "windows_code_build": False,
                 "exclude": EXCLUDE,
