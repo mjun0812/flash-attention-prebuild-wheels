5bc98e4b5d3e685aaa02d8d06e2cf6bb74c2a5e8
Junya Morioka <mjun@mjunya.com>
2025-12-16 20:52:14 +0900

    feat: update create_matrix.py for windows

--- create_matrix.py
+++ create_matrix.py
@@ -167,21 +167,22 @@ WINDOWS_MATRIX = {
 
 WINDOWS_CODEBUILD_MATRIX = {
     "flash-attn-version": [
-        "2.6.3",
-        "2.7.4.post1",
+        # "2.6.3",
+        # "2.7.4.post1",
         "2.8.3",
     ],
     "python-version": [
-        "3.10",
-        "3.11",
+        # "3.10",
+        # "3.11",
         "3.12",
-        "3.13",
+        # "3.13",
     ],
     "torch-version": [
         "2.9.1",
     ],
     "cuda-version": [
-        "13.0",
+        "12.8",
+        # "13.0",
     ],
 }
 
@@ -196,14 +197,14 @@ def main():
                 "linux_arm64": False,
                 # "linux_arm64": LINUX_ARM64_MATRIX,
                 #
-                # "linux_self_hosted": False,
-                "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
+                "linux_self_hosted": False,
+                # "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
                 #
-                # "linux_arm64_self_hosted": False,
-                "linux_arm64_self_hosted": LINUX_ARM64_SELF_HOSTED_MATRIX,
+                "linux_arm64_self_hosted": False,
+                # "linux_arm64_self_hosted": LINUX_ARM64_SELF_HOSTED_MATRIX,
                 #
-                "windows": False,
-                # "windows": WINDOWS_MATRIX,
+                # "windows": False,
+                "windows": WINDOWS_MATRIX,
                 #
                 "windows_code_build": False,
                 # "windows_code_build": WINDOWS_CODEBUILD_MATRIX,
