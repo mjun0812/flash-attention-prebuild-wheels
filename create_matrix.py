89bfe94ab1a0f19c8589ff4cb6f0d5cf12b9fb25
Junya Morioka <mjun@mjunya.com>
2025-12-21 11:22:06 +0900

    chore: Update create_matrix.py

--- create_matrix.py
+++ create_matrix.py
@@ -98,12 +98,21 @@ LINUX_SELF_HOSTED_MATRIX = {
         "3.10",
         "3.11",
         "3.12",
-        # "3.13",
+        "3.13",
         # "3.14",
     ],
-    "torch-version": ["2.9.1"],
+    "torch-version": [
+        "2.5.1",
+        "2.6.0",
+        "2.7.1",
+        "2.8.0",
+        # "2.9.1",
+    ],
     "cuda-version": [
+        "12.4",
+        "12.6",
         "12.8",
+        "12.9",
         "13.0",
     ],
 }
@@ -197,8 +206,8 @@ def main():
                 "linux_arm64": False,
                 # "linux_arm64": LINUX_ARM64_MATRIX,
                 #
-                "linux_self_hosted": False,
-                # "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
+                # "linux_self_hosted": False,
+                "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
                 #
                 "linux_arm64_self_hosted": False,
                 # "linux_arm64_self_hosted": LINUX_ARM64_SELF_HOSTED_MATRIX,
