169c3a97fed3003b344ed4324f27ea0baf98c0a4
Junya Morioka <mjun@mjunya.com>
2026-03-01 15:11:58 +0900

    feat: add Python 3.14 for Linux

--- create_matrix.py
+++ create_matrix.py
@@ -9,22 +9,24 @@ LINUX_MATRIX = {
         "2.8.3",
     ],
     "python-version": [
-        "3.10",
-        "3.11",
-        "3.12",
-        "3.13",
+        # "3.10",
+        # "3.11",
+        # "3.12",
+        # "3.13",
+        "3.14",
+        "3.14t",
     ],
     "torch-version": [
-        "2.5.1",
-        "2.6.0",
-        "2.7.1",
-        "2.8.0",
-        "2.9.1",
-        # "2.10.0",
+        # "2.5.1",
+        # "2.6.0",
+        # "2.7.1",
+        # "2.8.0",
+        # "2.9.1",
+        "2.10.0",
     ],
     "cuda-version": [
-        "12.4",
-        # "12.6",
+        # "12.4",
+        "12.6",
         "12.8",
         # "12.9",
         "13.0",
@@ -208,14 +210,14 @@ def main():
     print(
         json.dumps(
             {
-                "linux": False,
-                # "linux": LINUX_MATRIX,
+                # "linux": False,
+                "linux": LINUX_MATRIX,
                 #
-                # "linux_arm64": False,
-                "linux_arm64": LINUX_ARM64_MATRIX,
+                "linux_arm64": False,
+                # "linux_arm64": LINUX_ARM64_MATRIX,
                 #
-                # "linux_self_hosted": False,
-                "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
+                "linux_self_hosted": False,
+                # "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
                 #
                 "linux_arm64_self_hosted": False,
                 # "linux_arm64_self_hosted": LINUX_ARM64_SELF_HOSTED_MATRIX,
